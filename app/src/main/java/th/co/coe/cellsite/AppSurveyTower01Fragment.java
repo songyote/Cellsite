c87780036e598ec0d6965acb26bb8d6c77d34a81
songyote.m <songyote.m@gmail.com>
2015-12-08 20:26:39 +0700

    Initial commit

---
+++ app/src/main/java/th/co/coe/cellsite/AppSurveyTower01Fragment.java
@@ -0,0 +1,29 @@
+package th.co.coe.cellsite;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AppSurveyTower01Fragment extends Fragment {
+
+
+    public AppSurveyTower01Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_app_survey_tower01, container, false);
+    }
+
+}
---
+++ app/src/main/java/th/co/coe/cellsite/DataActivity.java
@@ -0,0 +1,22 @@
+package th.co.coe.cellsite;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class DataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
